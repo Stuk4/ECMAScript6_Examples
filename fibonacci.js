4a23e411b9c9c6b159f61eaa3cc8bcd7b81ef485
U-AXCESSFINANCIAL\clarico <clarico@AXF3452.axcess-financial.com>
2015-01-26 18:54:32 -0500

    Error Invalid left-hand

---
+++ fibonacci.js
@@ -0,0 +1,15 @@
+var fibonacci = function*(){
+	
+	let [prev, curr]= [0,1]
+	for(;;){
+		[prev, curr] = [curr, prev+curr];
+		yield curr;		
+	}
+
+}
+
+let seq = fibonacci();
+
+console.log(seq.next().value);
+console.log(seq.next().value);
+console.log(seq.next().value);
